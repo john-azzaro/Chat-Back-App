6ace07c1f026fbf7687d0ae586f7857940d15869
John Azzaro <jazzaro@temple.edu>
2019-12-31 21:06:42 -0800

    resolved append issue, added server.js etc.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const io = require('socket.io')(3000);
 
 io.on('connection', function(socket) {
-  console.log('new user');
-  socket.emit('chat-message', 'Hello world')
+  socket.on('send-chat-message', function(message) {
+    socket.broadcast.emit('chat-message', message);
+  });
 });
