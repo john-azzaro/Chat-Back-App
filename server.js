f3cc97b8de07a7284db8d5f5d2e069d77c702478
John Azzaro <jazzaro@temple.edu>
2019-12-31 20:46:33 -0800

    update dscript, server, added styles, HTML

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(3000);
 
-io.on('connection', socket => {
+io.on('connection', function(socket) {
   console.log('new user');
   socket.emit('chat-message', 'Hello world')
 });
