a9254abeab2beeccdf53f73827f9098e58de3138
John Azzaro <jazzaro@temple.edu>
2020-01-22 22:39:08 -0800

    testing user connection

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const io = require('socket.io')(3000);
 
 io.on('connection', function(socket) {
+  console.log('new user')
   socket.on('send-chat-message', function(message) {
-    socket.broadcast.emit('chat-message', message);
+    socket.broadcast.emit('chat-message', 'Hello world');
+    
   });
 });
