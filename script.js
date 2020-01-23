d4d0fd97ef4dc20cc39bf4e6b0646eab96561036
John Azzaro <jazzaro@temple.edu>
2020-01-22 22:45:58 -0800

    added append message

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
 
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
 
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
